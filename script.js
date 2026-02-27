2547ecab937e62f5e1652a34645086ae33c36689
rajrgs-dot <rajrgs@gmail.com>
2026-02-26 21:57:54 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll(".btn");
-
-buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        console.log("Link clicado:", btn.textContent);
-    });
-});
